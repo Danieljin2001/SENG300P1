cc0c54bba58f42db5e62f58e4b7a1b75e2f068db
Danieljin2001 <Djin2001@gmail.com>
2021-03-17 02:16:50 -0600

    changed the PayWithBanknote constructor. And added conditions in acceptBanknote.

--- PayWithBanknote/src/org/lsmr/p1/PayWithBanknote.java
+++ PayWithBanknote/src/org/lsmr/p1/PayWithBanknote.java
@@ -1,27 +1,70 @@
 package org.lsmr.p1;
 
+import java.util.Currency;
+
 import org.lsmr.selfcheckout.Banknote;
+import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.BanknoteValidator;
 import org.lsmr.selfcheckout.devices.DisabledException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.listeners.AbstractDeviceListener;
+import org.lsmr.selfcheckout.devices.listeners.BanknoteValidatorListener;
 
 //accepts one bank note at a time after validating it
 public class PayWithBanknote {
-	private BanknoteValidator banknoteValidator;
-	private int amountPaid;
+	private int amountPaid = 0;
+	private SelfCheckoutStation selfCheckoutStation;
+	private boolean banknoteIsValid = false;
 
 	/*Constructor: the banknoteValidator should be a constant therefore, the bankValidator should have
 	been initiated with what currency and denominations to accept beforehand*/
-	public PayWithBanknote(BanknoteValidator aBanknoteValidator) {
-		this.banknoteValidator = aBanknoteValidator;
-		this.amountPaid = 0;
+	public PayWithBanknote(SelfCheckoutStation aSelfCheckoutStation) {
+		this.selfCheckoutStation = aSelfCheckoutStation;
+		selfCheckoutStation.banknoteValidator.register(bvl);
 	}
+	
+	private BanknoteValidatorListener bvl = new BanknoteValidatorListener() {
+
+		@Override
+		public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void validBanknoteDetected(BanknoteValidator validator, Currency currency, int value) {
+			// TODO Auto-generated method stub
+			banknoteIsValid = true;
+			
+		}
+
+		@Override
+		public void invalidBanknoteDetected(BanknoteValidator validator) {
+			// TODO Auto-generated method stub
+			banknoteIsValid = false;
+			
+		}
+		
+	};
+	
 	
 	//if the bank note is accepted, it is delivered into the Unidirectional sink and amount paid is registered
 	//if the bank note is not accepted, it is ejected out of the same place the bank note was initially inserted into and amount paid is $0
 	public void acceptBanknote(Banknote aBanknote) {
 		try {
-			banknoteValidator.accept(aBanknote);
-			amountPaid = aBanknote.getValue();
+			selfCheckoutStation.banknoteValidator.accept(aBanknote);
+			if (selfCheckoutStation.banknoteStorage.hasSpace() && banknoteIsValid) { //checks if the sink has space and bank note is valid
+				amountPaid = aBanknote.getValue();
+			}
+			else {
+				amountPaid = 0;
+			}
 		}
 		catch (DisabledException e) {
 			amountPaid = 0;
